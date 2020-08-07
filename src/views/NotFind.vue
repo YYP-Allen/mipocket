30d805dc4e5c6b3041717d44d90e355f39d4877d
YYP-Allen <1519327651@qq.com>
2020-08-07 15:57:25 +0800

    添加 404 页面

---
+++ src/views/NotFind.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>抱歉！页面无法访问......</div>
+    <p>请检查网址是否正确</p>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFind'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
