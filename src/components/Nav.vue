1853d48e9881832897e1b7f528f7c68caf2c9814
YYP-Allen <1519327651@qq.com>
2020-08-10 22:55:34 +0800

    完成 HTML 与 CSS

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -24,8 +24,8 @@
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
   nav {
+    @extend %outerShadow;
     display: flex;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
     flex-direction: row;
     > .item {
       width: 33.333333%;
