bbd6d96a2b1164f2b2390560ac97fe07c4f2ebce
YYP-Allen <1519327651@qq.com>
2020-08-18 14:44:43 +0800

    改进 Notes.vue 组件，使其成为一个通用的组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,9 @@
   <Layout class-prefix="layout">
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
-    <Notes @update:value="onUpdateNotes"/>
+    <Notes field-name="备注:"
+           placeholder="~请输入备注~"
+           @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
